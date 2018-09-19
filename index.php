b42bf20da6bb2918129fcc3790d833ff79aef5ce
Charlotte <cha.perution@gmail.com>
2018-09-19 22:03:04 +0200

    add layout

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php 
+<?php
 
-?>
+// autoload les 2 contrôleurs 
+
+try {
+	if (isset($_GET['action'])) {
+		if ($_GET['action'] == 'accueil') {
+			// fonction du contrôleur
+		}
+	} else {
+		//fonction du contrôleur
+	}
+}
+catch(Exception $e) {
+	echo 'Erreur : ' . $e->getMessage();
+}
