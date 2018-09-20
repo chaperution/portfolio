7ae3e085d4936c057c98a68b2c1326ce0e998955
Charlotte <cha.perution@gmail.com>
2018-09-20 13:38:34 +0200

    create class Database in model

---
+++ app/model/Database.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace CP\portfolio;
+
+Abstract class Database {
+	protected $db;
+	private $_dbName = 'portfolio';
+	private $_dbUser = 'root';
+	private $_dbPass = '';
+	private $_dbHost = 'localhost';
+
+	public function __construct() {
+		$db = new PDO('mysql:host='. $this->_dbHost . ';dbname='. $this->_dbName .';charset=utf8', $this->_dbUser, $this->_dbPass);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		return $db;
+	}
+}
