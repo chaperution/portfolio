bf4bfa29e155ab7a68ef4e2cbaa3b85d83327e39
Charlotte <cha.perution@gmail.com>
2018-09-23 18:38:32 +0200

    add github API for events and CSS

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,76 +1,94 @@
 // Ajout de l'API Github avec profil utilisateur
 
-//var linkAPI = "https://api.github.com/users/" + userName;
-
-function Github(userName, APIFrame) {
-	this.userName = userName;
-	this.APIFrame = APIFrame;
-	this.login;
-	this.html_url;
-	this.avatar_url;
-	this.public_repos;
-	this.location;
+function Github(userName, profilAPI, eventsAPI) {
 
 	var that = this;
 
-	ajaxGet("https://api.github.com/users/" + userName, function(reponse){
-		var event = JSON.parse(reponse);
+	this.initProfil = function() {
+		ajaxGet("https://api.github.com/users/" + userName, function(reponse){
+			var event = JSON.parse(reponse);
 
-		that.login = event.login;
-		that.html_url = event.html_url;
-		that.avatar_url = event.avatar_url;
-		that.public_repos = event.public_repos;
-		that.location = event.location;
+			var avatarElt = document.createElement("img");
+			avatarElt.src = event.avatar_url;
+			avatarElt.style.height = "200px";
+			avatarElt.style.width = "200px";
+			avatarElt.style.borderRadius = "50%";
 
-		var avatarElt = document.createElement("img");
-		avatarElt.src = that.avatar_url;
-		avatarElt.style.height = "200px";
-		avatarElt.style.width = "200px";
-		avatarElt.style.borderRadius = "50%";
+			var userElt = document.createElement("p");
+			var urlElt = document.createElement("a");
+			userElt.appendChild(urlElt);	
+			urlElt.href = event.html_url;
+			urlElt.textContent = event.login;
+			urlElt.style.fontWeight = "bold";
+			urlElt.style.fontFamily = "Roboto";
+			urlElt.style.fontSize = "1.4em";
+			urlElt.style.textDecoration = "none";
+			urlElt.style.color = "#45aaf2";
+			
+			var locationElt = document.createElement("p");
+			locationElt.innerHTML = "<i class='fas fa-map-marker-alt' aria-hidden='true'></i>  ";
+			locationElt.appendChild(document.createTextNode(event.location));
+			locationElt.style.fontFamily = "Roboto";
+			locationElt.style.fontSize = "1.2em";
 
-		var userElt = document.createElement("p");
-		var urlElt = document.createElement("a");
-		userElt.appendChild(urlElt);	
-		urlElt.href = that.html_url;
-		urlElt.textContent = that.login;
-		urlElt.style.fontWeight = "bold";
-		urlElt.style.fontFamily = "Roboto";
-		urlElt.style.fontSize = "1.4em";
-		urlElt.style.textDecoration = "none";
-		urlElt.style.color = "#45aaf2";
-		
-		var locationElt = document.createElement("p");
-		locationElt.innerHTML = "<i class='fas fa-map-marker-alt' aria-hidden='true'></i>  ";
-		locationElt.appendChild(document.createTextNode(that.location));
-		locationElt.style.fontFamily = "Roboto";
-		locationElt.style.fontSize = "1.2em";
+			var reposElt = document.createElement("div");
+			if (that.public_repos !== 0) {
+				reposElt.innerHTML = "<i class='far fa-file-alt'></i>  ";
+				reposElt.appendChild(document.createTextNode(event.public_repos + " repositories"));
+				reposElt.style.fontFamily = "Roboto";
+				reposElt.style.fontSize = "1.2em";
+			} else {
+				reposElt.textContent = "Pas de repositories !"
+			}
+			
+			profilAPI.appendChild(avatarElt);
+			profilAPI.appendChild(userElt);
+			profilAPI.appendChild(locationElt);
+			profilAPI.appendChild(reposElt);
+		});
+	}
+	
+	this.initEvents = function() {
+		ajaxGet("https://api.github.com/users/" + userName + "/events", function(reponse){
+			var events = JSON.parse(reponse);
+			// filtre les éléments selon le type
+			events = events.filter(function(el){ return el.type == "PushEvent"; });
 
-		var reposElt = document.createElement("div");
-		if (that.public_repos !== 0) {
-			reposElt.innerHTML = "<i class='far fa-file-alt'></i>  ";
-			reposElt.appendChild(document.createTextNode(that.public_repos + " repositories"));
-			reposElt.style.fontFamily = "Roboto";
-			reposElt.style.fontSize = "1.2em";
-		} else {
-			reposElt.textContent = "Pas de repositories !"
-		}
-		
-		APIFrame.appendChild(avatarElt);
-		APIFrame.appendChild(userElt);
-		APIFrame.appendChild(locationElt);
-		APIFrame.appendChild(reposElt);
-	});
+			for (i = 0; i < 5 && i < events.length; i++) {
+				var dateEvent = document.createElement("em");
+				dateEvent.textContent = events[i].created_at;
+				dateEvent.style.fontFamily = "Roboto";
+
+				var nameRepo = document.createElement("p");
+				nameRepo.textContent = "Repository : " + events[i].repo.name;
+				nameRepo.style.fontFamily = "Roboto";
+				nameRepo.style.fontSize = "1.2em";
+				nameRepo.style.color = "#45aaf2";
+
+				var commits = events[i].payload.commits;
+				console.log(commits);
+				for (a = 0; a < commits.length; a++) {
+					var messageEvent = document.createElement("p");
+					messageEvent.textContent = "Commit : " + commits[a].message;
+					messageEvent.fontFamily = "Roboto";
+					messageEvent.style.fontSize = "1.2em";
+				}
+
+				eventsAPI.appendChild(nameRepo);
+				eventsAPI.appendChild(dateEvent);
+				eventsAPI.appendChild(messageEvent);	
+			}
+		});
+	}	
 }
 
-var myGithub = new Github("chaperution", document.getElementById("API"));
+var myGithub = new Github("chaperution", document.getElementById("profilAPI"), document.getElementById("eventsAPI"));
+myGithub.initProfil();
+myGithub.initEvents();
 
 
-console.log(reponse);
-
-//var APIFrame = document.getElementById("API");
-
-//var userName = "chaperution";
-
+//https://api.github.com/repos/chaperution/portfolio/events
+//https://api.github.com/users/chaperution/events
 
 //"https://api.github.com/users/" + userName + "/events/public";
 //"https://api.github.com/repos/" + userName + "/" + repoName + "/events"
